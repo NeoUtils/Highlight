18ac1c3866b01518237cecebae8ef052d150d33d
Irineu A. Silva <mmmirineusilva@gmail.com>
2024-10-13 15:42:22 -0300

    Improve build logic
    - Migrate to Gradle KTS #26
    - Update Gradle to 8.6
    - Update AGP to 8.5.2
    - Implement version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":highlight")
+
+rootProject.name = "Highlight"
