946da2f94d86590b71d6a125b417c2bab6b16e99
Irineu A. Silva <mmmirineusilva@gmail.com>
2024-10-17 22:23:31 -0300

    feat(compose): create highlight model

---
+++ example/compose/src/main/java/com/neoutils/highlight/example/compose/launcher/CodeHighlightExample.kt
@@ -0,0 +1,121 @@
+package com.neoutils.highlight.example.compose.launcher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.neoutils.highlight.compose.remember.rememberHighlightModel
+import com.neoutils.highlight.core.Highlight
+import com.neoutils.highlight.core.utils.UiColor
+import com.neoutils.highlight.example.compose.theme.HighlightTheme
+import com.neoutils.highlight.extension.highlight
+import org.intellij.lang.annotations.Language
+
+class CodeHighlightExample : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            HighlightTheme(darkTheme = false) {
+
+                val highlightOn = remember { mutableStateOf(true) }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {
+                                highlightOn.value = !highlightOn.value
+                            }
+                        ) {
+                            if (highlightOn.value) {
+                                Text("Off")
+                            } else {
+                                Text("On")
+                            }
+                        }
+                    }
+                ) { innerPadding ->
+
+                    val highlightModel = rememberHighlightModel(
+                        if (highlightOn.value) {
+                            CodeHighlight
+                        } else {
+                            Highlight()
+                        }
+                    )
+
+                    val textFieldValue by highlightModel.state.collectAsState()
+
+                    LaunchedEffect(Unit) {
+                        highlightModel.onChangeValue(
+                            TextFieldValue(code)
+                        )
+                    }
+
+                    BasicTextField(
+                        modifier = Modifier.padding(innerPadding),
+                        value = textFieldValue.copy(
+                            composition = null
+                        ),
+                        onValueChange = {
+                            highlightModel.onChangeValue(it)
+                        },
+                        decorationBox = {
+                            Box(Modifier.padding(8.dp)) {
+                                it()
+                            }
+                        },
+                        textStyle = TextStyle(
+                            fontSize = 16.sp
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+private val CodeHighlight = highlight {
+    textColor {
+        fully(
+            regex = "\\b(fun)\\b",
+            UiColor.Hex(hex = "#0033B3")
+        )
+
+        groups(
+            regex = "\\b(fun)\\b\\s*\\b(\\w+)\\b\\([^()]*\\)",
+            UiColor.Hex(hex = "#0033B3"),
+            UiColor.Hex(hex = "#00627A")
+        )
+
+        fully(
+            regex = "@.+",
+            UiColor.Hex(hex = "#93880D")
+        )
+
+        fully(
+            regex = "\"[^\"]*\"",
+            UiColor.Hex(hex = "#067D17")
+        )
+    }
+}
+
+@Language("kotlin")
+private val code = """
+    fun main() {
+        print("Hello, World!")
+    }
+""".trimIndent()
---
+++ highlight/compose/src/main/java/com/neoutils/highlight/compose/remember/HighlightModel.kt
@@ -0,0 +1,71 @@
+package com.neoutils.highlight.compose.remember
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.input.TextFieldValue
+import com.neoutils.highlight.compose.extension.toAnnotatedString
+import com.neoutils.highlight.core.Highlight
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+
+class HighlightModel(
+    highlight: Highlight
+) {
+
+    internal val scope = CoroutineScope(
+        context = SupervisorJob() + Dispatchers.Main.immediate
+    )
+
+    internal val highlight = MutableStateFlow(highlight)
+
+    private val textFieldValue = MutableStateFlow(TextFieldValue())
+
+    val state = combine(
+        this.highlight,
+        textFieldValue
+    ) { highlight, value ->
+        value.copy(
+            annotatedString = highlight.toAnnotatedString(
+                value.text
+            )
+        )
+    }.stateIn(
+        scope = scope,
+        initialValue = textFieldValue.value,
+        started = SharingStarted.WhileSubscribed()
+    )
+
+    fun onChangeValue(value: TextFieldValue) {
+        textFieldValue.value = value
+    }
+}
+
+@Composable
+fun rememberHighlightModel(
+    highlight: Highlight
+): HighlightModel {
+
+    val highlightModel = remember {
+        HighlightModel(highlight)
+    }
+
+    LaunchedEffect(highlight) {
+        highlightModel.highlight.value = highlight
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            highlightModel.scope.cancel()
+        }
+    }
+
+    return highlightModel
+}
